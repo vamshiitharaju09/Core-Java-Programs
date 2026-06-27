9cb978566124624562f7095f8c879b071a131e0d
Vamshi Itharaju <vamshiitharaju13@gmail.com>
2026-06-27 18:44:20 +0530

    Substraction Operation Completed

---
+++ src/operations/substraction.java
@@ -0,0 +1,18 @@
+package operations;
+
+public class substraction {
+	public static void main(String[]args)
+	{
+		//Substraction is used to for find  the diff b/w two operandas
+		
+		System.out.println(6-8);
+		
+		System.out.println(5-2.0);
+		
+		//System.out.println("hello" - 5); its compile time error.
+		
+		System.out.println('a'-10);
+		
+	}
+
+}
