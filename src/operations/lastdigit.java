f5087076ed5a8b2037a08a326b2a48eaff97aebf
Vamshi Itharaju <vamshiitharaju13@gmail.com>
2026-06-27 19:50:27 +0530

    Print last digit

---
+++ src/operations/lastdigit.java
@@ -0,0 +1,10 @@
+package operations;
+// write a java program to print last digit of the number
+public class lastdigit {
+  public static void main(String[]args)
+  {
+	  int num=573;
+	  int last=num%10;
+	  System.out.println("printing last digit from 573 is = " + last);
+  }
+}
