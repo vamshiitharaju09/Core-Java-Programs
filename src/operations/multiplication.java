46700816b6dc212a270b9092c48b2e2fd6a717c8
Vamshi Itharaju <vamshiitharaju13@gmail.com>
2026-06-27 19:34:14 +0530

    Multiplication Opearation Completed

---
+++ src/operations/multiplication.java
@@ -0,0 +1,13 @@
+package operations;
+
+public class multiplication {
+	public static void main(String[]args)
+	{
+		//Multiplication is used to multiplie the two operands
+		
+		System.out.println(7*8);
+		System.out.println(5*7);
+		//System.out.println("bye"*3);
+	}
+
+}
