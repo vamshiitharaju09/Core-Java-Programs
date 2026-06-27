94d1fb9098ab99fb7dabe35d48a95897f996e2dd
Vamshi Itharaju <vamshiitharaju13@gmail.com>
2026-06-27 19:38:29 +0530

    Division Operation Completed

---
+++ src/operations/division.java
@@ -0,0 +1,13 @@
+package operations;
+
+public class division {
+ public static void main(String[]args)
+ {
+	 // Division Operator is used to the Quotient
+	 
+	 System.out.println(16/2);
+	 System.out.println(5/2);
+	 System.out.println(9.0/2);
+	 System.out.println(10/6);
+ }
+}
