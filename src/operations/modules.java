cb30d865c488052c441a1e7e63655dcfdb3fef82
Vamshi Itharaju <vamshiitharaju13@gmail.com>
2026-06-27 19:41:49 +0530

    Modules Operation Completed

---
+++ src/operations/modules.java
@@ -0,0 +1,13 @@
+package operations;
+
+public class modules {
+  public static void main(String[]args)
+  {
+	  //Modules Operator is used to the remainder
+	  
+	  System.out.println(20%2);
+	  System.out.println(5%2);
+	  System.out.println(53%10);
+	  System.out.println(10%16);
+  }
+}
