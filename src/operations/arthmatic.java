079b1331da49ce3b2af381bbd9fe86b01eb3b8bd
Vamshi Itharaju <vamshiitharaju13@gmail.com>
2026-06-27 18:36:27 +0530

    Addition Operation Completed

---
+++ src/operations/arthmatic.java
@@ -0,0 +1,45 @@
+package operations;
+
+public class arthmatic {
+	public static void main(String[]args)
+	{
+		//Addition Operators
+		int a=10+24;
+		System.out.println(a);
+		System.out.println(30+67);
+		
+	 System.out.println("Integer + Integer =Integer");
+	  System.out.println(25+67);
+	  
+	  
+	 System.out.println("Integer + Decimal =Decimal");
+	  System.out.println(23+4.3);
+	  
+	  System.out.println("Deciaml + Integer =Decimal");
+	   System.out.println(3.2+56);
+	   
+	   System.out.println("Decimal + Decimal =Decimal");
+	   System.out.println(2.5+2.5);
+	  
+	   System.out.println("Integer + char =Integer");
+	   System.out.println(20+'a');
+	   
+	   System.out.println("char + Decimal =Decimal");
+	   System.out.println('b'+2.3);
+	   
+	   System.out.println("char + char =Integer");
+	   System.out.println('a'+'b');
+	   
+	   System.out.println("CONCATINATION");
+	    
+	   System.out.println("10"+30);
+	   System.out.println(2.5+"hello");
+	   System.out.println('v'+"bye");
+	   System.out.println("Prabhas"+"Anu");
+	   
+	   
+	   
+	
+	}
+
+}
