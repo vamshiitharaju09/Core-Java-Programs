69ceefa056c2524766222580150ac1f8e4965849
Vamshi Itharaju <vamshiitharaju13@gmail.com>
2026-06-27 19:46:12 +0530

    Removing last digit

---
+++ src/operations/removelastdigt.java
@@ -0,0 +1,10 @@
+package operations;
+
+public class removelastdigt {
+      public static void main(String[]args)
+      {
+    	  int a=123;
+    	  int b=a/10;
+    	  System.out.println("Removing last digit from 123 is =  "+ b);
+      }
+}
