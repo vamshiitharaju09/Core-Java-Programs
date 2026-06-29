e4047b1658c3519b6c869d0319b8e4b3e2e6f952
Vamshi Itharaju <vamshiitharaju13@gmail.com>
2026-06-29 23:02:26 +0530

    if-else-if

---
+++ src/conditionalStatement/IfElseIf.java
@@ -0,0 +1,28 @@
+package conditionalStatement;
+//When we have multiple conditions with their own implementation then we should go for else if else .
+public class IfElseIf {
+	public static void main(String[]args)
+	{
+		System.out.println("Start");
+		if(true)
+		{
+			System.out.println("JAVA");
+			
+		}
+		else if(true)
+		{
+			System.out.println("PYTHON");
+			
+		}
+		else if(true)
+		{
+			System.out.println("SQL");
+		}
+		else
+		{
+			System.out.println("HTML");
+		}
+		System.out.println("END");
+	}
+
+}
