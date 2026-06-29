43afa694d31190dbda93c57218715c52d8b05094
Vamshi Itharaju <vamshiitharaju13@gmail.com>
2026-06-29 20:02:44 +0530

    Dynamic Inputs

---
+++ src/dynamicInputs/dynamicinputs.java
@@ -0,0 +1,16 @@
+package dynamicInputs;
+// The process of taking different inputs during execution of java program is known as dynamic inputs.
+//In java we can take dynamic inputs with help of Scanner class.
+// Scanner is a predefine class which is present in java.util package.
+import java.util.Scanner;   //---->import Statement
+public class dynamicinputs {
+	public static void main(String[]args)
+	{
+		System.out.println("Enter a number");
+		Scanner sc=new Scanner(System.in); //----> Object Creation
+		int a=sc.nextInt(); //----> Calling  Non Static Method
+		System.out.println("The dynamic input is : " + a);
+		
+	}
+
+}
