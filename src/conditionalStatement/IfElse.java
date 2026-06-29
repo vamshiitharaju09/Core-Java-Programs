6ac0833b5393426e472d373a701facfc51127d27
Vamshi Itharaju <vamshiitharaju13@gmail.com>
2026-06-29 22:12:47 +0530

    if-else condition

---
+++ src/conditionalStatement/IfElse.java
@@ -0,0 +1,20 @@
+package conditionalStatement;
+//if-else
+//else is a keyword which is used to create a else block.
+//else block will be executed when if condition is false.
+//with help of if else block we can achieve both positive and negative scenarios.
+// one if block should contain only one else block.
+public class IfElse {
+public static void main(String[]args)
+{
+	System.out.println("Start");
+	if(false)
+	{
+		System.out.println("RCB");
+	}
+	else
+	{
+		System.out.println("SRH");
+	}
+}
+}
