24043346aa83875904785a97f803f505fa4ec027
Vamshi Itharaju <vamshiitharaju13@gmail.com>
2026-06-30 22:04:42 +0530

    for loop task 1

---
+++ src/LoopingStatement/ForLoop1.java
@@ -0,0 +1,12 @@
+package LoopingStatement;
+//Write a java program to print the natural numbers from 1 to 10
+public class ForLoop1 {
+	public static void main(String[]args)
+	{
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println(i);
+		}
+	}
+
+}
