ea6d0fc32ca4b101436b6231b3fa76f9d5783da9
Vamshi Itharaju <vamshiitharaju13@gmail.com>
2026-06-30 22:01:37 +0530

    for loop

---
+++ src/LoopingStatement/ForLoop.java
@@ -0,0 +1,14 @@
+package LoopingStatement;
+//When we know how many times to repeat the task,then we can go for "for loop".
+// Q)Write a java program to print java for five times.
+public class ForLoop {
+	public static void main(String[]args)
+	{
+		for(int i=1;i<=5;i++)
+		{
+			System.out.println("JAVA");
+			
+		}
+	}
+
+}
