0252a14f1fc9a8ebd63701c49683fc663029edd4
Vamshi Itharaju <vamshiitharaju13@gmail.com>
2026-06-30 22:20:11 +0530

    print even number using for loop

---
+++ src/LoopingStatement/ForLoop3.java
@@ -0,0 +1,12 @@
+package LoopingStatement;
+//write a java program to print even numbers between 2 to 10
+public class ForLoop3 {
+	public static void main(String[]args)
+	{
+		for(int i=2;i<=10;i=i+2)
+		{
+			System.out.println(i);
+		}
+	}
+
+}
