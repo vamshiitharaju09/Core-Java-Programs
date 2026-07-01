aea1e286b4d3b9f809a9d79c25124acf5eb6f036
Vamshi Itharaju <vamshiitharaju13@gmail.com>
2026-07-01 12:34:02 +0530

    While loop Task 1

---
+++ src/LoopingStatement/WhileLoop1.java
@@ -0,0 +1,13 @@
+package LoopingStatement;
+//Write a java program to print the natural numbers from 10 to 1 by using while loop.
+public class WhileLoop1 {
+ public static void main(String[]args)
+ {
+	 int i=10;
+	 while(i>=0)
+	 {
+		 System.out.println(i);
+		 i--;
+	 }
+ }
+}
