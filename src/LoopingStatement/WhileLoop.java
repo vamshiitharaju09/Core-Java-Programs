88505b25248b5e25439769c60251d2c81408464a
Vamshi Itharaju <vamshiitharaju13@gmail.com>
2026-07-01 12:31:18 +0530

    While loop

---
+++ src/LoopingStatement/WhileLoop.java
@@ -0,0 +1,15 @@
+package LoopingStatement;
+//While loop is keeps on executing until the condition false.
+//When we know and when we don't know how many times to repeat, the task then we can go for while looping statement.
+public class WhileLoop {
+	public static void main(String[]args)
+	{
+		int i=1;
+		while(i<=10)
+		{
+			System.out.println(i);
+			i++;
+		}
+	}
+
+}
