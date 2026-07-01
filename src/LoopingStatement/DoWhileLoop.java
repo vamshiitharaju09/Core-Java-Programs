aef113fd861abf0c7308a2121ac3d78dc6cfdf96
Vamshi Itharaju <vamshiitharaju13@gmail.com>
2026-07-01 13:09:01 +0530

    do while loop

---
+++ src/LoopingStatement/DoWhileLoop.java
@@ -0,0 +1,15 @@
+package LoopingStatement;
+//Do while loop is similar to the while loop ,which is keeps on executing until the condition fixed.
+// Here first it will execute set of instruction then it will check the condition.
+public class DoWhileLoop {
+	public static void main(String[]args)
+	{
+		int i=1;
+		do {
+			System.out.println(i);
+			i++;
+		}
+		while(i<=5);
+	}
+
+}
