8a30b2d2840a67bd120c02559845d70582c5f92f
Vamshi Itharaju <vamshiitharaju13@gmail.com>
2026-07-03 22:10:37 +0530

    without return type without formal argument'

--- src/Methods/Method.java
+++ src/Methods/Method.java
@@ -1,5 +1,11 @@
 package Methods;
 
 public class Method {
+	public static void main(String[]args)
+	{
+		//Method is block of instruction which is used to perform specific task.
+		//In java methods will executed only when we call the methods.
+		//we can call the methods with help of method calling statement.
+	}
 
 }
---
+++ src/Methods/NRNFA.java
@@ -0,0 +1,20 @@
+package Methods;
+//Without Return type without formal argument.
+public class NRNFA {
+	public static void main(String[]args)
+	{
+		System.out.println("Main started");
+		add();
+		System.out.println("Main Ended");
+	}
+	public static void add()
+	{
+		System.out.println("Add method start");
+		int a=10; 
+		int b=20;
+		System.out.println("The add two :" + (a+b));
+		System.out.println("Add method ended");
+		
+	}
+
+}
