b2d6c430f5f48ab197c9112c30a5663f5531eb59
Vamshi Itharaju <vamshiitharaju13@gmail.com>
2026-07-04 20:18:00 +0530

    Static Method

---
+++ src/MembersOfClass/StaticMethod.java
@@ -0,0 +1,25 @@
+package MembersOfClass;
+
+import Methods.Demo;
+
+//Any method which is declared with help of static keyword is known as "static method"
+public class StaticMethod {
+	public static void view()
+	{
+		System.out.println("RCB");
+	}
+	public static void print() {
+		System.out.println("SRH");
+	}
+	public static void main(String[]args)
+	{
+		//Direct Access
+		view();
+		print();
+		
+		//class name and dot operator
+		
+		StaticMethod.view();
+		StaticMethod.print();
+	}
+}
--- src/MembersOfClass/StaticVariable.java
+++ src/MembersOfClass/StaticVariable.java
@@ -1,5 +1,21 @@
 package MembersOfClass;
-
+//When the properties and behaviours of the object is cammon for all the objects ,then we should go for "static Members"
+//In java we can create static members with help of static keyword.
+//static is a keyword and modifier which is used to create static variables, static methods, and static intializer.
 public class StaticVariable {
+//-->Any variable which is declared inside the class by using static keyword is known as "static variable".
+//-->In java all the static variables are the initializer with default values.
+//--->In java static variables can also possible to access in different class.
+	 static double a;
+	 static boolean b;
+	 static char c;
+	 public static void main(String[]args)
+	 {
+		 System.out.println(a);
+		 System.out.println(StaticVariable.b);
+		 System.out.println(c);
+	 }
 
 }
+
+
