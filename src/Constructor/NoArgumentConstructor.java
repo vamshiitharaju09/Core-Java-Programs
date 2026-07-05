3a8eaf21aac113a5882cf13270e6a18cb1ead7aa
Vamshi Itharaju <vamshiitharaju13@gmail.com>
2026-07-05 21:43:32 +0530

    No argument constructor

---
+++ src/Constructor/NoArgumentConstructor.java
@@ -0,0 +1,17 @@
+package Constructor;
+//A constructor which does not have any formal arguments is known as "No Argument Constructor".
+public class NoArgumentConstructor {
+	NoArgumentConstructor(){
+		System.out.println("CS");
+		System.out.println("Vamshi");
+		System.out.println("CE");
+	}
+	public static void main(String[]args)
+	{
+		System.out.println("MS");
+		NoArgumentConstructor r=new NoArgumentConstructor();
+		System.out.println("ME");
+	}
+	
+
+}
