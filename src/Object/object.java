482c7b8264fd7a54f0703b13c4f2fdcfdf2956ee
Vamshi Itharaju <vamshiitharaju13@gmail.com>
2026-07-05 19:31:06 +0530

    Object and Keyword

---
+++ src/Object/object.java
@@ -0,0 +1,18 @@
+package Object;
+//Object is a block of memory which is used to store the multiple variables and multiple methods.
+//To represent the real world entity into software world we need object.
+public class object {
+	public static void main(String[]args)
+	{
+		System.out.println("Main Start");
+		//new is a keyword which is used to "create the object" inside the heap.
+		//At the end new keyword will return "object address".
+		System.out.println(new object());
+		System.out.println(new object());
+		System.out.println("Main Ended");
+		
+		
+	
+	}
+
+}
