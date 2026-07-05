1a2f18a725efd65607d882dc514b12de9c36520b
Vamshi Itharaju <vamshiitharaju13@gmail.com>
2026-07-05 20:10:13 +0530

    Accessing the non static method from Demo class

---
+++ src/MembersOfClass/Demo.java
@@ -0,0 +1,13 @@
+package MembersOfClass;
+//Accessing the non static method from one class to different class .
+public class Demo {
+	public void dancing()
+	{
+		System.out.println("person is dacing");
+	}
+	public void view()
+	{
+		System.out.println("person is seeing");
+	}
+
+}
---
+++ src/MembersOfClass/person.java
@@ -0,0 +1,11 @@
+package MembersOfClass;
+//Accessing the non static method from Demo class. 
+public class person {
+	public static void main(String[]args)
+	{
+		Demo d1=new Demo();
+		d1.dancing();
+		d1.view();
+	}
+
+}
