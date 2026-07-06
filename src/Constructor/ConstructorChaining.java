d90711ff7feb3f7027d4f597f9c7ec820616e915
Vamshi Itharaju <vamshiitharaju13@gmail.com>
2026-07-06 22:42:49 +0530

    Constructor chaining with this calling statement

---
+++ src/Constructor/ConstructorChaining.java
@@ -0,0 +1,34 @@
+package Constructor;
+//The process of one constructor calling another constructor is known as "Constructor Chaining".
+//In java we can achieve constructor chaining in two ways.
+//i)this calling statement.
+//ii)super calling statement.
+
+// i) this calling statement.
+// this calling statement is used to call another constructor which is present in same class.
+public class ConstructorChaining {
+	ConstructorChaining()
+	{
+		System.out.println("SRH");
+	}
+	ConstructorChaining(int a)
+	{
+		this();
+		System.out.println("RCB");
+	}
+	ConstructorChaining(double a)
+	{
+		this(10);
+		System.out.println("PBK");
+	}
+	public static void main(String[]args)
+	{
+		ConstructorChaining c1=new ConstructorChaining();
+		System.out.println("----------------------");
+		ConstructorChaining c2=new ConstructorChaining(10);
+		System.out.println("----------------------");
+		ConstructorChaining c3=new ConstructorChaining(2.4);
+	}
+	
+
+}
