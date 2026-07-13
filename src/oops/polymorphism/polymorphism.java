759cc25b056a05ae3ae9f5e7fe7b1bc4e14dcc3e
Vamshi Itharaju <vamshiitharaju13@gmail.com>
2026-07-13 20:38:03 +0530

    polymorphism and compile time polymorphism

--- src/oops/polymorphism/polymorphism.java
+++ src/oops/polymorphism/polymorphism.java
@@ -1,5 +1,23 @@
 package oops.polymorphism;
 
 public class polymorphism {
+	
 
+//--> The members of class showing different behaviour with same name is known as "Polymorphism".
+//--> In java we can write two different types of programs.
+	//i)Normal Program.
+	//ii)Polymorphism.
+	
+//--> In all the polymorphism programs when programmer is accessing the members internally two components will take decision.
+//-->Based on this polymorphism is classified into two types.
+//   i)Compile Time polymorphism.
+  // ii)Runtime polymorphism.
+   
+ //  i)Compile Time Polymorphism.
+  // -->In Polymorphism program the decision is taken by compiler during compile time is known as "Compile time polymorphism" and it is also static binding.
+     
+    //    Example : i)Constructor Overloading
+      //            ii)Method Overloading
+
+	
 }
