fc6b029a9309b2c07c4786bb6200f57ee54cb300
Vamshi Itharaju <vamshiitharaju13@gmail.com>
2026-07-13 11:51:05 +0530

    Non-Primitive type casting

--- src/oops/inheritance/HasARelation/Aggregation.java
+++ src/oops/inheritance/HasARelation/Aggregation.java
@@ -1,5 +1,16 @@
 package oops.inheritance.HasARelation;
 
 public class Aggregation {
+//If an object is partially depending on another object is known as "Aggregation".
+//One object can exists without another object.
+// In java we can achieve aggregation by using "Lazy Instantiation".
+	
+	//How to achieve composition by using "Lazy instantiation"
+		//step-1:Create blue print for dependent object.
+		//step-2:Create blue print for depending object.
+		//step-3:create dependent type of object reference variable inside depending class.
+		//step-4:Create object for dependent class and initialize object address inside the non-static method (this process is Lazy instantiation.
+	    //step-5:Create the object for depending class and access the properties of both the class.
+	}
 
-}
+
---
+++ src/oops/inheritance/typeCasting/NonPrimitiveTypeCasting.java
@@ -0,0 +1,5 @@
+package oops.inheritance.typeCasting;
+
+public class NonPrimitiveTypeCasting {
+
+}
