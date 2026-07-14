f79e7f543930ddf020ceeb7aadab576a73302d79
Vamshi Itharaju <vamshiitharaju13@gmail.com>
2026-07-14 12:15:20 +0530

    The role of constructor during inheritance

---
+++ src/oops/abstraction/Demo2.java
@@ -0,0 +1,9 @@
+package oops.abstraction;
+//The role of constructor during inheritance
+abstract class Demo2 {
+	Demo2()
+	{
+		System.out.println("Abstract class Constructor");
+	}
+
+}
---
+++ src/oops/abstraction/Test2.java
@@ -0,0 +1,15 @@
+package oops.abstraction;
+
+public class Test2 extends Demo2 {
+	Test2()
+	{
+		super();
+		System.out.println("Concrete class constructor");
+	}
+	public static void main(String[]args)
+	{
+		Test2 t1=new Test2();
+		
+	}
+
+}
