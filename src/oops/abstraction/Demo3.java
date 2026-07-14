a5ec49f09690cc96f6c9799b87683667dd5c5402
Vamshi Itharaju <vamshiitharaju13@gmail.com>
2026-07-14 12:19:32 +0530

    Creating and accessing abstract method inside abstract class

---
+++ src/oops/abstraction/Demo3.java
@@ -0,0 +1,6 @@
+package oops.abstraction;
+//Creating and accessing abstract method inside abstract class 
+abstract class Demo3 {
+	public abstract void sample();
+
+}
---
+++ src/oops/abstraction/Test3.java
@@ -0,0 +1,15 @@
+package oops.abstraction;
+
+public class Test3 extends Demo3 {
+	public void sample()
+	{
+		System.out.println("Sample Method");
+	}
+	
+	public static void main(String[]args)
+	{
+		Test3 t3=new Test3();
+		t3.sample();
+	}
+
+}
