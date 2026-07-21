61472f8d5af4db622a0eeef01bb0878ec250ac3a
Vamshi Itharaju <vamshiitharaju13@gmail.com>
2026-07-21 20:26:52 +0530

    store person object into the arrays and print all details of the person

---
+++ src/Array/personObject.java
@@ -0,0 +1,28 @@
+package Array;
+//Write a java program to store person object into the arrays and print all details of the person
+public class personObject {
+	String name;
+	int age;
+	personObject(String name,int age)
+	{
+		this.name=name;
+		this.age=age;
+	}
+	public String toString()
+	{
+		return "Name : "+name+"\n Age : "+age+"\n";
+	}
+	public static void main(String[]args)
+	{
+		personObject p1=new personObject("Vamshi",21);
+		personObject p2=new personObject("Shiva",31);
+		personObject p3=new personObject("Ram",27);
+		personObject[]p= {p1,p2,p3};
+		for(int i=0;i<p.length;i++)
+		{
+			System.out.println(p[i]);
+		}
+		
+	}
+
+}
