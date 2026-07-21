d3eccc3fef2fd291c9c3355bb3d7be7e59d53832
Vamshi Itharaju <vamshiitharaju13@gmail.com>
2026-07-21 19:38:08 +0530

    Array

---
+++ src/Array/array.java
@@ -0,0 +1,16 @@
+package Array;
+//Array is a continuous block of memory which is used to store multiple data.
+//Array is fixed in size.
+//Array is homogenous data.
+//Array is index based.
+//Duplicates are allowed in array.
+public class array {
+	public static void main(String[]args)
+	{
+		int []arr= {10,20,30};
+		System.out.println(arr[0]);
+		System.out.println(arr[1]);
+		System.out.println(arr[2]);
+	}
+
+}
