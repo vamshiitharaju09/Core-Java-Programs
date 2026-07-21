20060c40b564675ac6083eb22b9538840a2b1f69
Vamshi Itharaju <vamshiitharaju13@gmail.com>
2026-07-21 18:02:42 +0530

    Unchecked Exception

---
+++ src/ExceptionHandling/UnCheckedException.java
@@ -0,0 +1,6 @@
+package ExceptionHandling;
+//UnChecked Exception is a compiler Unknown exception, Hence compiler is not forcing you to handle the exception.
+public class UnCheckedException {
+	//Ex;Arthmatic Exception
+
+}
